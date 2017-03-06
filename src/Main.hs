29a988ee76a41a577e41013b06abba6013b6f55c
Anthony Nguyen <anknguyen@gmail.com>
2017-03-06 10:30:08 -0500

    Add base64 decoding function

--- src/Main.hs
+++ src/Main.hs
@@ -1,32 +1,49 @@
 module Main where
 
-import Data.Char (digitToInt,intToDigit,ord)
+import Data.Char (chr,digitToInt,intToDigit,ord)
+import Data.List (elemIndex)
 import Numeric (readInt,showIntAtBase)
 
 main :: IO ()
 main = do
     putStrLn $ base64encode "test"
+    putStrLn $ show $ base64decode "dGVzdA=="
+    putStrLn $ show $ base64decode "YXNkZg=="
+    putStrLn $ show $ base64decode "YXNkZmZ6cA=="
 
 --------------------------------------------------------------------------------
 
 base64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 
 base64encode :: String -> String
-base64encode xs = unpad $ map (base64chars !!) $ map unbits6 $ splitEvery 6
-                  $ concat $ bits8 $ map ord $ xs ++ (replicate pad '\0')
+base64encode xs = unpad $ map (base64chars !!) $ map unbits $ splitEvery 6
+                  $ concat $ bitsn 8 $ map ord $ xs ++ (replicate pad '\0')
     where r = length xs `rem` 3
           pad
             | r == 1    = 2
             | r == 2    = 1
             | otherwise = 0
-          unpad :: String -> String
           unpad xs = reverse $ (replicate pad '=') ++ (drop pad $ reverse xs)
 
-bits8 :: [Int] -> [String]
-bits8 = map lpad . map (\x -> showIntAtBase 2 intToDigit x "")
+base64decode :: String -> Maybe String
+base64decode xs
+  | any (not . (flip elem) ('=':base64chars)) xs = Nothing
+  | otherwise = Just $ unpad $ map chr $ map unbits $ splitEvery 8 
+                $ concat $ bitsn 6 $ map charToIndex xxs
+    where pad = length $ takeWhile (== '=') $ reverse xs
+          xxs = reverse $ (replicate pad 'A') ++ (drop pad $ reverse xs)
+          unpad = reverse . drop pad . reverse
+
+-- this function should only be used once we've verified our string
+charToIndex :: Char -> Int
+charToIndex c = val
+    where (Just val) = elemIndex c base64chars
+
+bitsn :: Int -> [Int] -> [String]
+bitsn n xs = map lpad $ map (\x -> showIntAtBase 2 intToDigit x "") xs
     where lpad :: String -> String
           lpad xs
-            | length xs >= 8 = xs
+            | length xs >= n = xs
             | otherwise      = lpad ('0':xs)
 
 splitEvery :: Int -> [a] -> [[a]]
@@ -34,5 +51,5 @@ splitEvery _ [] = []
 splitEvery n xs = f:(splitEvery n s)
     where (f, s) = splitAt n xs
 
-unbits6 :: String -> Int
-unbits6 = fst . head . readInt 2 (`elem` "01") digitToInt
+unbits :: String -> Int
+unbits = fst . head . readInt 2 (`elem` "01") digitToInt
